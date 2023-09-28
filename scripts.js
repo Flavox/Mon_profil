ec90853bb053b3570ff706ef16369e2cd57a2ef0
Flavox <31452144+Flavox@users.noreply.github.com>
2023-09-28 11:26:07 +0200

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,19 @@
+//const cafe = document.querySelector(cafe)
+//cafe.addEventListener("click", () => {
+//    cafe.innerText = "&#127866;"
+//})
+
+//const etvous = document.querySelector(etvous)
+
+//etvous.addEventListener("click", () => {
+//    etvous.innerText = "et vous aussi !"
+//})
+
+
+let btn = document.querySelector("#projets");
+console.log(btn.innerText)
+
+
+// btn.addEventListener("click", () => {
+//     btn.innerText = "testé :)"
+// })
